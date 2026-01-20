abb08c34abf96d59ec82ae5933ca782657040f04
Luhc011 <lucashcardoso26@gmail.com>
2026-01-20 12:31:53 -0300

    f pr

---
+++ src/PocTresCamadas.Api/Controllers/FornecedoresControler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PocTresCamadas.Api.ViewModels;
+
+namespace PocTresCamadas.Api.Controllers;
+
+public class FornecedoresControler : MainController
+{
+    public FornecedoresControler()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+    {
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+    {
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
+    {
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
+    {
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+    {
+    }
+
+}
